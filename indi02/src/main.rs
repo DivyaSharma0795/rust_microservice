f6b5efb174e2a2f7ac5d6801d8b69856b08c223e
Divya <divya.sharma@duke.edu>
2024-03-25 00:31:41 -0400

    changed rust

--- indi02/src/main.rs
+++ indi02/src/main.rs
@@ -3,7 +3,10 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .map(|name: String| {
+            let reversed = name.chars().rev().collect::<String>();
+            format!("Hello, {}! Your name in reverse is {}.", name, reversed)
+        });
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
